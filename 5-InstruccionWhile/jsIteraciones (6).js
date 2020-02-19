cfb8ba3a38101fd4a5a40a9bfe4371704ba0d6bf
alumno <cabezaleandro@gmail.com>
2020-02-18 22:15:27 -0300

    do while

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -3,10 +3,17 @@ function mostrar()
 
 	var contador=0;
 	var acumulador=0;
+	var numero;
 
-		while(){
-			
-		}
+
+// contador incremente en valores fijos ej ;  +1 , +2 , etc  . 
+// ACUMULADOR incrementa en valores VARIABLES EJ ;  numero  + variable.
+		  while (contador < 5 ){
+		  	numero = parseInt(prompt("ingrese un número "));
+		  	acumulador = acumulador + numero ; 
+		  	contador++ ; 
+		  }
+
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/5;
--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -3,8 +3,33 @@ function mostrar()
 
 	var contador=0;
 	var acumulador=0;
-	var respuesta='si';
+	var respuesta="si";
 
+		/*while (respuesta == "si"){
+		  	numero = parseInt(prompt("ingrese un número "));
+		  	acumulador = acumulador + numero ; 
+		  	respuesta = prompt("Quieres seguir sumando ?") ; 
+		  	contador ++ ; 
+		  }
+			*/
+
+		  do{
+		  	numero = parseInt(prompt("ingrese un número "));
+		  	acumulador = acumulador + numero ; 
+		  	respuesta = prompt("Quieres seguir sumando ?") ; 
+		  	contador ++ ;
+		  	 
+		  }while(respuesta== "si") ; 
+
+		 /*
+
+		 do {
+				alert("hola");
+				seguir = prompt("Quiere que lo salude de nuevo ?");
+		 }while (seguir == " s ")
+
+
+		 */
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/contador;
