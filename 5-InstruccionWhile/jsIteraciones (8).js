cf83875f027cb027bd85f5272784b9c0b30c96da
LeanCabeza <cabezaleandro@gmail.com>
2020-02-19 20:06:22 -0300

    clase19/02/2020

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -1,11 +1,34 @@
 function mostrar()
 {
-
-	var contador=0;
 	var positivo=0;
 	var negativo=1;
-	
+	var numero;
 	var respuesta='si';
+	var flag ;
+
+	 do{
+
+	 	numero = parseInt(prompt("Ingrese un numero"));
+
+	 	while(isNaN(numero)){
+	 		numero=parseInt(prompt("Eso no es un numero ,Ingrese un numero "));
+	 	}	
+
+	 	if( numero >= 0 ){
+	 		positivo = positivo + numero ; 
+
+	 	}else {
+	 		negativo = negativo * numero ;
+	 		flag = 1 ; 
+	 	}
+
+	 	respuesta= prompt("Quiere contunuar ingresando numeros ?");
+
+	 }while(respuesta== "si" );
+
+	 if (flag == 0 ) {
+	 	negativo = 0 ;
+	 }
 
 
 document.getElementById('suma').value=positivo;
