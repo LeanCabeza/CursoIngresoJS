387683ce8c916aa93c8db2257d33af0aba859743
LeanCabeza <cabezaleandro@gmail.com>
2020-02-19 22:14:00 -0300

    ejercicio 10 aterminar

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,16 +1,46 @@
 function mostrar()
 {
 
-	var contador=0;
-	//declarar contadores y variables 
-	
-	var respuesta="si";
+	var respuesta;
+	var numero ;
+	var sumaNegativos = 0 ;
+	var contadorNegativos= 0 ;
+	var sumaPositivos = 0   ; 
+	var contadorPositivos = 0; 
+	var contadorDeCeros = 0 ;
 
-	while(respuesta!="no")
-	{
-		
+	do{
+		numero = parseInt(prompt("Ingrese un Numero")); 
+
+		while(isNaN(numero)){
+		 		numero=parseInt(prompt("Eso no es un numero ,Ingrese un numero "));
+		 	}
+
+
+		 	if (numero < 0  ){
+		 		sumaNegativos =  sumaNegativos + numero ;
+		 		contadorNegativos ++ ;
+		 	}else if (numero > 0) {
+		 		sumaPositivos = sumaPositivos + numero ; 
+		 		contadorPositivos ++ ;
+		 	}else{
+		 		contadorDeCeros ++ ;
+		 	}
+
+
+		respuesta= prompt("Quiere contunuar ingresando numeros ?");
 	
-	}
+	}while(respuesta == "si");
+
+
+
+	//  IF (nroPares %2 == 0 ) , esto sifgnifica si el esto da 0 , ES PAR.
+
+		document.write("La sumade los negativos es " + sumaNegativos + "<br>" );
+		document.write("La sumade los positovs es " + sumaPositivos + "<br>" );
+		document.write("La cantidad de nros negativos ingresados es " + contadorNegativos + "<br>" );
+		document.write("La cantidad de nros positivos ingresados es " + contadorPositivos + "<br>" );
+		document.write("La cantidad de Ceros es " + contadorDeCeros + "<br>" );
 
 
 
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,17 +1,54 @@
 function mostrar()
 {
 
-	var contador=0;
-	// declarar variables
-	
-	var respuesta='si';
+	var flag=0;
+	var respuesta ;
+	var numero ; 
+	var maximo ;
+	var minimo ; 
 
-	while(respuesta!='no')
+	do
 	{
-		
-	
-	}
+		numero = parseInt(prompt("Ingrese un Numero")); 
 
+		while(isNaN(numero)){
+		 		numero=parseInt(prompt("Eso no es un numero ,Ingrese un numero "));
+		 	}	
+
+		 // FORMA ACOTADA
+
+		 if (flag == 0 || numero > maximo ){
+		 	maximo = numero ;
+		 }
+
+		 if (flag == 0 || numero < minimo){
+		 	minimo = numero ;
+		 	flag = 1 ;
+		 }
+
+
+		/*  FORMA CLASICA 
+
+		if (flag == 0) {
+		 		maximo = numero ;
+		 		minimo = numero ; 
+		 		flag = 1 ;
+		 	}else{
+					if ( numero > maximo  ){
+						maximo = numero ; 
+					}
+
+						else if ( numero < minimo ){
+						minimo = numero ;
+					} */
+					
+
+		respuesta= prompt("Quiere contunuar ingresando numeros ?");
+
+	}while(respuesta == "si"); 
+
+	document.getElementById('maximo').value=maximo;
+	document.getElementById('minimo').value=minimo;
 
 
 
